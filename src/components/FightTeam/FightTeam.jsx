1fdc6bcefe0fc611ee34d1f7962a304b6d871350
chris <jimenezchristopher5@gmail.com>
2024-06-20 20:56:10 -0500

    completed styling of FightTeam component

--- src/components/FightTeam/FightTeam.jsx
+++ src/components/FightTeam/FightTeam.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { SuperHeroesContext } from "../../App";
 import { useNavigate } from "react-router-dom";
+import styles from "./FightTeam.module.css";
 
 const FightTeam = () => {
 
@@ -8,18 +9,19 @@ const FightTeam = () => {
     const navigate = useNavigate();
 
   return (
-    <>
-        <h1>Fight Team</h1>
+
+    <div className={styles.mainContainer}>
+        <h1 className={styles.title}>Fight Team</h1>
         {superheroTeam.length === 0 ? <h2>-- Empty, please select SuperHeroes --</h2> 
         :
             <ol>
         {superheroTeam.map((superhero, index)=>{
             return(
-                <li key={index}>
+                <li key={index} className={styles.listItems}>
                     <dt>{superhero.superName}</dt>
-                    <dd>Strength: {superhero.strength}</dd>
-                    <dd>Power: {superhero.superPower}</dd>
-                    <button onClick={()=> removeHeroFromTeam(index)}>-</button>
+                    <dd style={{color: "white"}}>Strength: <span style={{color: "lightgreen"}}>{superhero.strength}</span></dd>
+                    <dd style={{color: "white"}}>Power: <span style={{color: "lightblue"}}>{superhero.superPower}</span></dd>
+                    <button onClick={()=> removeHeroFromTeam(index)} className={styles.button}>-</button>
                 </li>
             )
         })}
@@ -28,9 +30,9 @@ const FightTeam = () => {
         
 
         {superheroTeam.length === 4 && (
-            <button onClick={()=> {navigate("/battle")}}>ready</button>
+            <button onClick={()=> {navigate("/battle")}} className={styles.button}>ready</button>
         )}
-    </>
+    </div>
 
   )
 }
